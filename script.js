282e168e69d92033a2a33b32ff700343f78b4235
Karthick R <106647257+Karthick-code@users.noreply.github.com>
2024-05-14 16:48:25 +0200

    Updating changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function details() {
-    var f_name = document.getElementById("Firstname").value;
+    var f_name = document.getElementById("FirstName").value;
     console.log("First name:", f_name);
-    var m_name = document.getElementById("Middlename").value;
+    var m_name = document.getElementById("MiddleName").value;
     console.log("Middle name:", m_name);
-    var l_name = document.getElementById("Lastname").value;
+    var l_name = document.getElementById("LastName").value;
     console.log("Last name:", l_name);
     var email = document.getElementById("Email").value;
     console.log("Email:", email);
